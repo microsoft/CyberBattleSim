c8ee55de2f9fb610f794c2c803636b7521f6aa2a
William Blum <william.blum@microsoft.com>
2022-09-08 13:02:36 -0700

    Fix two bugs caught by pyright - Make `FirewallConfiguration` dataclass and `FirewallRule` frozen dataclass - Boolean value not properly returned by __mark_discovered_nodes - discovered nodes values should not be added to the reward unless the nodes just got owned

--- notebooks/run_all.sh
+++ notebooks/run_all.sh
@@ -6,6 +6,12 @@ run () {
 }
 
 mkdir output -p
+
+run notebook_benchmark-toyctf
+run notebook_benchmark-chain
+run notebook_benchmark-tiny
+run notebook_dql_transfer
+
 run c2_interactive_interface
 run chainnetwork-optionwrapper
 run chainnetwork-random
@@ -13,3 +19,9 @@ run randomnetwork
 run toyctf-blank
 run toyctf-random
 run toyctf-solved
+
+run dql_active_directory
+run notebook_randlookups
+run notebook_tabularq
+run notebook_withdefender
+run random_active_directory
