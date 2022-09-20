dc349f1b5bf6210ee3a4bf77fc54b5551719a1ce
William Blum <william.blum@microsoft.com>
2022-09-20 11:07:39 -0700

    fix typing issue in running_mean

--- cyberbattle/agents/baseline/plotting.py
+++ cyberbattle/agents/baseline/plotting.py
@@ -131,10 +131,10 @@ def plot_all_episodes_loss(all_episodes_losses, name, label):
     plt.plot(x, all_episodes_losses, label=f'{name} {label}')
 
 
-def running_mean(x: np.ndarray, size):
+def running_mean(x, size):
     """return moving average of x for a window of lenght 'size'"""
     cumsum = np.cumsum(np.insert(x, 0, 0))
-    return (cumsum[size:] - cumsum[:-size]) / float(size)
+    return np.subtract(cumsum[size:], cumsum[:-size]) / float(size)
 
 
 class PlotTraining:
