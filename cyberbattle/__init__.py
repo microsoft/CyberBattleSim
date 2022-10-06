be408777d9ba5b285c7cc3c84da690eb068a9433
Andrei Kvasov <andrei.kvasov.dl@gmail.com>
2022-10-06 17:12:02 +0000

    PR for #87 choice 1: Gym register new environments with version 0 preserving same AttackerGoal as before

--- cyberbattle/__init__.py
+++ cyberbattle/__init__.py
@@ -44,8 +44,23 @@ register(
     cyberbattle_env_identifiers=toy_ctf.ENV_IDENTIFIERS,
     entry_point='cyberbattle._env.cyberbattle_toyctf:CyberBattleToyCtf',
     kwargs={'defender_agent': None,
-            'attacker_goal': AttackerGoal(own_atleast=6),
+            'attacker_goal': AttackerGoal(own_atleast=6, own_atleast_percent=1.0),
             'defender_goal': DefenderGoal(eviction=True)
+            },
+    # max_episode_steps=2600,
+)
+
+if 'CyberBattleToyCtf-v1' in registry.env_specs:
+    del registry.env_specs['CyberBattleToyCtf-v1']
+
+register(
+    id='CyberBattleToyCtf-v1',
+    cyberbattle_env_identifiers=toy_ctf.ENV_IDENTIFIERS,
+    entry_point='cyberbattle._env.cyberbattle_toyctf:CyberBattleToyCtf',
+    kwargs={'defender_agent': None,
+            'attacker_goal': AttackerGoal(own_atleast=6),
+            'defender_goal': DefenderGoal(eviction=True),
+            'winning_reward': 0.0
             },
     # max_episode_steps=2600,
 )
@@ -58,7 +73,7 @@ register(
     cyberbattle_env_identifiers=toy_ctf.ENV_IDENTIFIERS,
     entry_point='cyberbattle._env.cyberbattle_tiny:CyberBattleTiny',
     kwargs={'defender_agent': None,
-            'attacker_goal': AttackerGoal(own_atleast=6),
+            'attacker_goal': AttackerGoal(own_atleast=6, own_atleast_percent=1.0),
             'defender_goal': DefenderGoal(eviction=True),
             'maximum_total_credentials': 10,
             'maximum_node_count': 10
@@ -66,6 +81,21 @@ register(
     # max_episode_steps=2600,
 )
 
+if 'CyberBattleTiny-v1' in registry.env_specs:
+    del registry.env_specs['CyberBattleTiny-v1']
+
+register(
+    id='CyberBattleTiny-v1',
+    cyberbattle_env_identifiers=toy_ctf.ENV_IDENTIFIERS,
+    entry_point='cyberbattle._env.cyberbattle_tiny:CyberBattleTiny',
+    kwargs={'defender_agent': None,
+            'attacker_goal': AttackerGoal(reward=1000),  # , own_atleast_percent=0.0),  # (own_atleast=6)
+            'defender_goal': DefenderGoal(eviction=True),
+            'maximum_total_credentials': 10,
+            'maximum_node_count': 10,
+            },
+    max_episode_steps=25,
+)
 
 if 'CyberBattleRandom-v0' in registry.env_specs:
     del registry.env_specs['CyberBattleRandom-v0']
